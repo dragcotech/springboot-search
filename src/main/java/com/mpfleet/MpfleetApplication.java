a6c7b037743c943af9c571cd3a5176c2e1135c24
Martin Parunev <parunev@gmail.com>
2023-03-06 13:11:36 +0200

    setting up the project adding the template

---
+++ src/main/java/com/mpfleet/MpfleetApplication.java
@@ -0,0 +1,13 @@
+package com.mpfleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpfleetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpfleetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mpfleet/MpfleetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpfleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpfleetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
